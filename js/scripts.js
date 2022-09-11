6307b4d20fccb24bc0941614ccfc5d7e205d7544
Emzzy241 <emzzyoluwole@gmail.com>
2022-09-11 17:22:43 +0100

    writing vanilla js for game

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
