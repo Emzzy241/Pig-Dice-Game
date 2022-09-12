c53c3b2895a7c4f1ee06a7ab5815d222e5458f47
Emzzy241 <emzzyoluwole@gmail.com>
2022-09-12 05:25:48 +0100

    writing a new function for newGame

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,15 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-let scores = [0, 0];
-let currentscore = 0;
-let activeplayer = 0;
 
-const switchPlayer = function(){
+const newGame = function () {
+
+    let scores = [0, 0];
+    let currentscore = 0;
+    let activeplayer = 0;
+}
+
+const switchPlayer = function () {
     document.getElementById(`current--${activeplayer}`).textContent = 0;
     currentscore = 0;
     activeplayer = activeplayer === 0 ? 1 : 0;
@@ -23,27 +27,27 @@ const switchPlayer = function(){
     player1El.classList.toggle('player--active')
 }
 
-btnRoll.addEventListener('click', function(){
+btnRoll.addEventListener('click', function () {
     const dice = Math.trunc(Math.random() * 6) + 1;
     diceEl.remove('hidden')
     diceEl.src = `./img/dice${dice}.png`
-    if(dice !== 1){
+    if (dice !== 1) {
         currentscore += dice
         document.getElementById(`current--${activeplayer}`).textContent = currentscore;
-    } else{
+    } else {
         switchPlayer
     }
 })
 
-btnHold.addEventListener('click', function(){
+btnHold.addEventListener('click', function () {
     scores[activeplayer] += currentscore;
     document.getElementById(`score--${activeplayer}`).textContent = scores[activeplayer]
-    if( scores[activeplayer >= 20]){
+    if (scores[activeplayer >= 20]) {
         diceEl.classList.add('hidden');
         document.querySelector(`.player--${activeplayer}`).classList.add('player--active')
         document.querySelector(`.player--${activeplayer}`).classList.remove('player--active')
         document.querySelector('body').styles.backgroundColor = '#64b347'
-    } else{
+    } else {
         switchPlayer()
     }
 })
