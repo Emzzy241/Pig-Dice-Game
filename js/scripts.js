82a4d767d6c07365ac811352d75e2b18787640ae
Emzzy241 <emzzyoluwole@gmail.com>
2022-09-12 05:35:05 +0100

    done with the newGame function

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentscore, activeplayer, playing;
 
 const newGame = function () {
 
-    let scores = [0, 0];
-    let currentscore = 0;
-    let activeplayer = 0;
-}
+    scores = [0, 0];
+    currentscore = 0;
+    activeplayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden')
+    player0El.classList.remove('player--winner')
+    player1El.classList.remove('player--winner')
+    player0El.classList.add('player--active')
+    player0El.classList.remove('player--active')
+    document.querySelector('body').styles.backgroundColor = '#c7365f'
+}   
+newGame();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activeplayer}`).textContent = 0;
